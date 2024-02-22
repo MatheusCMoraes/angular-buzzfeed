790c6e6083d72ed0ed62389ac233845f30a69dc5
Matheus Moraes <matheurmoraes635@gmail.com>
2024-02-22 16:28:29 -0300

    adicionado lógica

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz_questions from '../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
@@ -24,6 +25,48 @@ export class QuizzComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz_questions){
+      this.finished = false;
+      this.title = quizz_questions.title;
+
+      this.questions = quizz_questions.questions;
+      this.questionSelected = this.questions[this.questionIndex];
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+    }
+  }
+
+  playerChoice(valor:string){
+    this.answers.push(valor);
+
+    this.nextStep()
+  }
+
+ async nextStep(){
+    this.questionIndex +=1;
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex]
+    }else{
+      const finalAnswer:string = await this.checkResult(this.answers)
+      this.finished =true;
+      this.answerSelected = quizz_questions.results[finalAnswer as keyof typeof quizz_questions.results]
+    }
+  }
+
+  async checkResult(anwsers:string[]){
+    const result = anwsers.reduce((previous, current, index, array)=>{
+      if(
+        array.filter(item => item === previous).length >
+        array.filter(item => item === current).length  ){
+            return previous;
+      }else{
+        return current;
+      }
+    })
+
+    return result
   }
 
 }
